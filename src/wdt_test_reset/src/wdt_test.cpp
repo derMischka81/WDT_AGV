18dafb03260c0ab3c490d943d208eca4358044d3
NULL <null@null>
2018-10-18 20:18:32 +0800

    test finished

--- src/wdt_test_reset/src/wdt_test.cpp
+++ src/wdt_test_reset/src/wdt_test.cpp
@@ -18,10 +18,17 @@ extern "C" {
 #include "wdt_dio.h"
 }
 
+//BOOL __cdecl InitWDT(void);
+//BOOL __cdecl StopWDT(void);
+//BOOL __cdecl StartWDT(void);
+//BOOL __cdecl SetWDT(WORD tick, BYTE unit);
+//BOOL __cdecl ResetWDT(void);
+
+
 using namespace std;
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "ur_twist_server");
+    ros::init(argc, argv, "agv_wdt_reset");
     ros::NodeHandle nh;
     ros::Rate loop_rate(1);
     ros::NodeHandle n;
@@ -51,13 +58,26 @@ int main(int argc, char **argv) {
 //    StopWDT();
 //    return 0;
 
+
 //while (ros::ok()) {
     while (1) {
         ROS_INFO("%d seconds passed", ++i);
-        if (!SetWDT(5, 1)) {
-            printf("ResetWDT <-- ERROR\n");
-            return -1;
-        }
+    if (!StopWDT()) {
+        printf("StartWDT <-- ERROR\n");
+        return -1;
+    }
+    if (!ResetWDT()) {
+        printf("StartWDT <-- ERROR\n");
+        return -1;
+    }
+if (!SetWDT(5, 1)) {
+        printf("SetWDT <-- ERROR\n");
+        return -1;
+    }
+    if (!StartWDT()) {
+        printf("StartWDT <-- ERROR\n");
+        return -1;
+    }
         ROS_INFO("reset\n");
 
         ros::spinOnce();
